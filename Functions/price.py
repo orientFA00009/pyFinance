57774dfd5ea964461db698763be98f03edf10b45
orientFA00009 <41346091+orientFA00009@users.noreply.github.com>
2023-06-02 10:59:53 -0500

    Update price.py

--- Functions/price.py
+++ Functions/price.py
@@ -1,4 +1,4 @@
-#API key: PDOIX0V358OJSYIW alpha_vantage PDOIX0V358OJSYIW
+
 import requests as rq
 def get_price(str):
     req = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol='+str+'&apikey=PDOIX0V358OJSYIW'
