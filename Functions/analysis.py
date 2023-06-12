024fcde5ccca6e3f54804414bbd61b567ac4e0aa
orientFA00009 <41346091+orientFA00009@users.noreply.github.com>
2023-06-12 15:35:23 -0500

    Update analysis.py
    Created list of tuples to store data; new field dataTuple which is a list of tuple n = 5, allows for me to choose which aspect of the data I would like to graph

--- Functions/analysis.py
+++ Functions/analysis.py
@@ -4,9 +4,13 @@ class Analysis:
         #self.interval = interval
         self.name = name
         self.datadic = datadic
-        self.open = []
-        self.close = []
+        #hold all relevant data(open,close,high,low in a list of tuple)
+        #self.open = []
+        self.dataTuple = []
+        #self.close = []
         self.dates = []
+        self.data1 = []
+        self.data2 = []
    # def get_interval(self):
     #    return self.interval
     #def set_interval(self, new_interval):
@@ -19,9 +23,17 @@ class Analysis:
         return self.datadic
     #will clean data before processing in matplotlib/pandas, data will be in a list
     #from weekly time series, grab opening values and closing values
+    #tuple holds values (date,open,high,low,close,volume)
     def cleaner(self):
         for k,v in self.datadic.items():
             self.dates.append(k)
-            self.open.append(float(v['1. open']))
-            self.close.append(float(v['4. close']))
-            
+            self.dataTuple.append((float(v['1. open']),float(v['2. high']),float(v['3. low']),float(v['4. close']),int(v['5. volume'])))
+
+    def fill_data(self,index,index1):
+        list1 = []
+        list2 = []
+        for val in self.dataTuple:
+            list1.append(val[index])
+            list2.append(val[index1])
+        self.data1 = list1
+        self.data2 = list2
